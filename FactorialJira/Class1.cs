43b770ce601e01aa5504f04cdc98d5f1156deb48
Craig Douglass <cdouglass88@hotmail.com>
2020-03-04 20:18:52 -0800

    Factorial Function

--- FactorialJira/Class1.cs
+++ FactorialJira/Class1.cs
@@ -4,5 +4,15 @@ namespace FactorialJira
 {
     public class Class1
     {
+        public static int Factorial(int n)
+        {
+            if (n == 0) return 0;
+            if (n < 0 || n > 11) throw new ArgumentOutOfRangeException();
+            int product = n;
+            while (n > 1) {
+                product *= n - 1;
+            }
+            return product;
+        }
     }
 }
