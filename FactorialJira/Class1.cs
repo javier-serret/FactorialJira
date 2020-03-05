ace3a4fc135610bc0beeefc2d501c7aded91b2f3
Javier Lopez Serret <j.lopez@globant.com>
2020-03-04 21:54:59 -0600

    Initial check-in of module FactorialJira

---
+++ FactorialJira/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FactorialJira
+{
+    public class Class1
+    {
+    }
+}
---
+++ FactorialJiraConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactorialJiraConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
