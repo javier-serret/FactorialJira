d6eaf769d9656796446338717e60c51caa9510fc
Veronica Canizares <v.canizares@globant.com>
2020-03-11 19:14:52 -0700

    Console tests

--- FactorialProjectConsoleTests/UnitTest1.cs
+++ FactorialProjectConsoleTests/UnitTest1.cs
@@ -1,14 +1,1 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace FactorialProjectConsoleTests
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            //test
-        }
-    }
-}
+using System.Diagnostics; using Microsoft.VisualStudio.TestTools.UnitTesting;  namespace FactorialProjectConsoleTests {     [TestClass]     public class UnitTest1     {         [TestMethod]         public void TestMethod1()         {             // Part 1: use ProcessStartInfo class.             ProcessStartInfo startInfo = new ProcessStartInfo();             startInfo.CreateNoWindow = false;             startInfo.UseShellExecute = false;             startInfo.FileName = "FactorialJiraConsole.exe";             startInfo.WindowStyle = ProcessWindowStyle.Hidden;              // Part 2: set arguments.             startInfo.Arguments = "0";              // Part 3: start with the info we specified.             // ... Call WaitForExit.             using (Process exeProcess = Process.Start(startInfo))             {                     exeProcess.WaitForExit();                     Assert.AreEqual(1, exeProcess.StandardOutput.ReadToEnd());             }                      }     } } 
