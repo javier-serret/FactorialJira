024c8ee6ac176e30e18595eda8a1583eee61e46a
Javier Lopez Serret <j.lopez@globant.com>
2020-03-11 21:23:36 -0600

    Commiting

--- FactorialJiraUnitTests/UnitTest1.cs
+++ FactorialJiraUnitTests/UnitTest1.cs
@@ -20,37 +20,37 @@ namespace FactorialJira
             Assert.AreEqual(1, Class1.Factorial(1));
         }
 
-        //[TestMethod]
-        //public void Factorial_6()
-        //{
-        //    Assert.AreEqual(720, Class1.Factorial(6));
-        //}
+        [TestMethod]
+        public void Factorial_6()
+        {
+            Assert.AreEqual(720, Class1.Factorial(6));
+        }
 
-        //[TestMethod]
-        //public void Factorial_11()
-        //{
-        //    Assert.AreEqual(39916800, Class1.Factorial(11));
-        //}
+        [TestMethod]
+        public void Factorial_11()
+        {
+            Assert.AreEqual(39916800, Class1.Factorial(11));
+        }
 
-        //[TestMethod]
-        //public void Factorial_12()
-        //{
-        //    Assert.AreEqual(479001600, Class1.Factorial(12));
-        //}
+        [TestMethod]
+        public void Factorial_12()
+        {
+            Assert.AreEqual(479001600, Class1.Factorial(12));
+        }
 
-        //[TestMethod]
-        //[ExpectedException(typeof(ArgumentOutOfRangeException))]
-        //public void Factorial_negative_1()
-        //{
-        //    Class1.Factorial(-1);
-        //}
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Factorial_negative_1()
+        {
+            Class1.Factorial(-1);
+        }
 
-        //[TestMethod]
-        //[ExpectedException(typeof(ArgumentOutOfRangeException))]
-        //public void Factorial_13()
-        //{
-        //    Class1.Factorial(13);
-        //}
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Factorial_13()
+        {
+            Class1.Factorial(13);
+        }
     }
 }
 
